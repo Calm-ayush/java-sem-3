bb39171d7dc586bf772adb2bb28a968a81fee1f7
Calm-ayush <ayushrollno5@gmail.com>
2026-07-02 10:12:10 +0530

    new

---
+++ class3/function.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class function {
+    static int test(int a){
+        a++;
+        return a;
+    }
+    public static void main(String[] args){
+        Scanner ob = new Scanner(System.in);
+        System.out.println("enter a num");
+        int a = ob.nextInt();
+        System.out.println("output before function " + a);
+        a=test(a);
+        System.out.println("output after function " + a);
+        ob.close();
+    }
+}
